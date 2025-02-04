ca085c518c5fd6b88f7851587757a9b50a742953
obadoni2 <obadoniemma@gmail.com>
2025-02-04 14:30:38 +0100

    jkj

--- 0-making_change.py
+++ 0-making_change.py
@@ -1,27 +1,27 @@
-#!/usr/bin/ python3
-"""contain makeChange function"""
+#!/usr/bin/python3
+
+""" Contains makeChange function"""
+
 
 def makeChange(coins, total):
     """
-   Returns: fewest number of coins needed to meet total
+    Returns: fewest number of coins needed to meet total
         If total is 0 or less, return 0
         If total cannot be met by any number of coins you have, return -1
-    
     """
-    if not coin or coins is None:
-        return -1 
-    if  total <=0:
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
         return 0
-    
     change = 0
-    coin = sorted(coins)[::-1]
+    coins = sorted(coins)[::-1]
     for coin in coins:
         while coin <= total:
-           total -= coin
-           change += 1
+            total -= coin
+            change += 1
         if (total == 0):
             return change
-    return -1 
+    return -1
         
         
     
